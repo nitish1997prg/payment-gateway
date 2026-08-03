c57821747955b4cc8165358278a505b1f55dc470
Nitish Viraktamath <mathnitish1997@gmail.com>
2026-08-03 16:59:06 +0530

    Added dlq service functions

---
+++ webhook-service/services/dlqService.js
@@ -0,0 +1,65 @@
+import { webhookDeliveryQueue } from "../queue/webhookQueue.js";
+
+export async function getFailedJobs(){
+    const jobs = await webhookDeliveryQueue.getFailed();
+
+    return jobs.map(job => ({
+        jobId: job.id,
+        jobName: job.name,
+        attemptsMade: job.attemptsMade,
+        failedReason: job.failedReason,
+        paymentId: job.data.data.paymentId,
+        referenceId: job.data.data.referenceId,
+        eventType: job.data.eventType
+    }));
+}
+
+export async function retryFailedJob(jobId){
+
+    const job = await webhookDeliveryQueue.getJob(jobId);
+
+    if(!job){
+        throw new Error("Job not found!");
+    }
+
+    const state = await webhookDeliveryQueue.getJobState(jobId);
+
+    if(state !== "failed"){
+        throw new Error(`Job is currently in state: ${state}`);
+    }
+
+    await job.retry();
+
+    return {
+        jobId: job.id,
+        status: "requeued"
+    }
+}
+
+export async function deleteFailedJob(jobId){
+    
+    const job = await webhookDeliveryQueue.getJob(jobId);
+
+    if(!job){
+        throw new Error("Job not found!");
+    }
+
+    const state = await webhookDeliveryQueue.getJobState(jobId);
+
+    if(state !== "failed"){
+        throw new Error(`Job is currently in state: ${state}`);
+    }
+
+    await job.remove();
+
+    return {
+        jobId: job.id,
+        status: "deleted"
+    };
+}
+
+export const DlqService = {
+    getFailedJobs,
+    retryFailedJob,
+    deleteFailedJob
+};
